1259d49fb6b877dff0d5414ae4e7c3d1833d6d82
Inan Biswas <inanb2005@gmail.com>
2026-08-01 12:31:43 +0530

    Create Initial Project Architecture

---
+++ src/main.py
@@ -0,0 +1,27 @@
+"""
+Lumora AI
+----------
+Project Entry Point
+
+Author : Inan Biswas
+Project: Lumora AI
+
+"""
+
+def display_banner ():
+    # Display the Lumora AI startup banner.
+
+    print ("="*60)
+    print (f"                  Welcome to Lumora AI")
+    print ()
+    print (f"Conversations that Care. Technology that Understands.")
+    print ("="*60)
+
+
+def main_function ():
+    # Main entry point of the application.
+
+    display_banner ()
+
+if __name__ == "__main__":
+    main_function ()
